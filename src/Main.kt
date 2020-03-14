63bd1dfcff84e13ab1e9dc809da37a95cfe17965
Mikhail <mikha-khar@yandex.ru>
2020-03-14 23:03:09 +0700

    1)Создал enum  с флагами параметров командной строки 2) Изменил "захардкоженные" значения на значение созданного enum

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,17 @@
 import domain.InputArgs
+import enum.ConsoleFlag.*
 
 fun main(args: Array<String>) {
     val inputArgs = InputArgs()
     for (arg in args.indices) {
         when (args[arg]) {
-            "-login" -> inputArgs.login = args[arg + 1]
-            "-pass" -> inputArgs.password = args[arg + 1]
-            "-res" -> inputArgs.resource = args[arg + 1]
-            "-role" -> inputArgs.role = args[arg + 1]
-            "-ds" -> inputArgs.dateStart = args[arg + 1]
-            "-de" -> inputArgs.dateEnd = args[arg + 1]
-            "-vol" -> inputArgs.volume = args[arg + 1]
+            LOGIN.nameParam -> inputArgs.login = args[arg + 1]
+            PASSWORD.nameParam -> inputArgs.password = args[arg + 1]
+            RESOURCE.nameParam -> inputArgs.resource = args[arg + 1]
+            ROLE.nameParam -> inputArgs.role = args[arg + 1]
+            DATE_START.nameParam -> inputArgs.dateStart = args[arg + 1]
+            DATE_END.nameParam -> inputArgs.dateEnd = args[arg + 1]
+            VOLUME.nameParam -> inputArgs.volume = args[arg + 1]
         }
     }
     println(inputArgs)
---
+++ src/enum/ConsoleFlag.kt
@@ -0,0 +1,11 @@
+package enum
+
+enum class ConsoleFlag(val nameParam: String) {
+    LOGIN("-login"),
+    PASSWORD("-pass"),
+    RESOURCE("-res"),
+    ROLE("-role"),
+    DATE_START("-ds"),
+    DATE_END("-de"),
+    VOLUME("-vol")
+}
