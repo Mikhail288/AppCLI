98c63e9a3b6ee3745c0e0167a848e3961b3123f6
Mikhail <mikha-khar@yandex.ru>
2020-03-14 22:45:41 +0700

    Дополнил объект InputArgs.kt остальными аргументами и заполнил его полностью

--- src/Main.kt
+++ src/Main.kt
@@ -5,12 +5,12 @@ fun main(args: Array<String>) {
     for (arg in args.indices) {
         when (args[arg]) {
             "-login" -> inputArgs.login = args[arg + 1]
-            "-pass" -> println(args[arg + 1])
-            "-res" -> println(args[arg + 1])
-            "-role" -> println(args[arg + 1])
-            "-ds" -> println(args[arg + 1])
-            "-de" -> println(args[arg + 1])
-            "-vol" -> println(args[arg + 1])
+            "-pass" -> inputArgs.password = args[arg + 1]
+            "-res" -> inputArgs.resource = args[arg + 1]
+            "-role" -> inputArgs.role = args[arg + 1]
+            "-ds" -> inputArgs.dateStart = args[arg + 1]
+            "-de" -> inputArgs.dateEnd = args[arg + 1]
+            "-vol" -> inputArgs.volume = args[arg + 1]
         }
     }
     println(inputArgs)
--- src/domain/InputArgs.kt
+++ src/domain/InputArgs.kt
@@ -1,3 +1,11 @@
 package domain
 
-data class InputArgs(var login: String? = null)
+data class InputArgs(
+    var login: String? = null,
+    var password: String? = null,
+    var resource: String? = null,
+    var role: String? = null,
+    var dateStart: String? = null,
+    var dateEnd: String? = null,
+    var volume: String? = null
+)
