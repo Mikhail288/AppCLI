3c35d99b3e30134a924d72640013fc2cd7e6b377
Mikhail <mikha-khar@yandex.ru>
2020-03-14 22:31:06 +0700

    1) Создал вспомагательный объект InputArgs.kt, для приема входных аргументов 2) Попробовал передать аргумент login в объект и вывести его содержимое

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,18 @@
+import domain.InputArgs
+
 fun main(args: Array<String>) {
-    for(arg in args.indices){
-        when(args[arg]){
-            "-login" -> println(args[arg+1])
-            "-pass" -> println(args[arg+1])
+    val inputArgs = InputArgs()
+    for (arg in args.indices) {
+        when (args[arg]) {
+            "-login" -> inputArgs.login = args[arg + 1]
+            "-pass" -> println(args[arg + 1])
+            "-res" -> println(args[arg + 1])
+            "-role" -> println(args[arg + 1])
+            "-ds" -> println(args[arg + 1])
+            "-de" -> println(args[arg + 1])
+            "-vol" -> println(args[arg + 1])
         }
     }
+    println(inputArgs)
+
 }
---
+++ src/domain/InputArgs.kt
@@ -0,0 +1,3 @@
+package domain
+
+data class InputArgs(var login: String? = null)
