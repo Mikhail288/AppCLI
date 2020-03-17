3cbe7a7c74e6211ee7cd68e4dddb41b19f6d51f9
Mikhail <mikha-khar@yandex.ru>
2020-03-17 22:24:09 +0700

    R1.5 1)Поправил работу метода валидации логина и роли

--- src/util/ArgsValidator.kt
+++ src/util/ArgsValidator.kt
@@ -1,31 +1,24 @@
 package util
-
-import domain.InputArgs
 import enum.ExitCode
 import enum.Role
 import java.lang.IllegalArgumentException
 import kotlin.system.exitProcess
 
-fun validateLogin(login: String): Int {
+fun validateLogin(login: String) {
     val regex = "[a-z]{1,9}".toRegex()
-    if (!regex.matches(login)) {
+    if (!regex.matches(login))
         exitProcess(ExitCode.INVALID_LOGIN.codeNumber)
-    }
-    exitProcess(ExitCode.SUCCESS.codeNumber)
 }
 
 fun validateRole(role: String) {
-
     try {
-        if (Role.values().contains(Role.valueOf(role))) {
-            exitProcess(ExitCode.SUCCESS.codeNumber)
+        if (!Role.values().contains(Role.valueOf(role))){
+            exitProcess(ExitCode.UNKNOWN_ROLE.codeNumber)
         }
-    } catch (e: IllegalArgumentException) {
-        exitProcess(ExitCode.UNKNOWN_ROLE.codeNumber)
+    }
+    catch (e : IllegalArgumentException){
+            exitProcess(ExitCode.UNKNOWN_ROLE.codeNumber)
     }
 }
 
-    /*if (!Role.values().contains(Role.valueOf(role))) {
-        exitProcess(ExitCode.UNKNOWN_ROLE.codeNumber)
-    }
-exitProcess(ExitCode.SUCCESS.codeNumber)*/
+
