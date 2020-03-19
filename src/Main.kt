dd80cc86d7527adadcf9040a2ba9f107a14523fb
Shapovalova Vera <17ametista17@gmail.com>
2020-03-20 00:48:35 +0700

    5.5 Добавлена проверка пароля

--- src/Main.kt
+++ src/Main.kt
@@ -15,7 +15,7 @@ fun main(args: Array<String>) {
 
     val isAutentificationArgs = args[0] =="-log" && args[2] == "-pass"
     val isLogin = "vasya"
-    val isPassword = args[3].isNotEmpty()
+    val isPassword = "123"
 
 
 }
