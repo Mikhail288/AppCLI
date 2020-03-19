29bcda272246fe599685d3171f4607295ac8ac5c
Mikhail <mikha-khar@yandex.ru>
2020-03-20 00:02:01 +0700

    4.4 Добавлено условие для вызова справки

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,20 @@
+import kotlin.system.exitProcess
+
 fun main(args: Array<String>) {
     val isArgs = args.isNotEmpty()
     //println(args[0])
-    if(!isArgs || args[0]=="-h"){
+    if(args[0]=="-h"){
         outputHelp()
+        exitProcess(1)
     }
+    if (!isArgs){
+        outputHelp()
+        exitProcess(0)
+    }
+
+
 }
 fun outputHelp(){
     println("Это справка!!!")
 }
+
