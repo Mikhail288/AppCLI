4e56c8d6b5d332c011a19376da230858c419a767
Shapovalova Vera <17ametista17@gmail.com>
2020-03-22 00:23:12 +0700

    8.1 Добавлены поля ds, de, vol

--- src/domain/ArgsHandler.kt
+++ src/domain/ArgsHandler.kt
@@ -5,5 +5,8 @@ data class ArgsHandler(
         var login: String?,
         var password: String?,
         var resource: String?,
-        var role: String?
-)
+        var role: String?,
+        var dateStart: String?,
+        var dateEnd: String?,
+        var volune: String?
+        )
