dc8e0f75fe4b2ca82918060845cb62608910eb02
Shapovalova Vera <17ametista17@gmail.com>
2020-03-21 22:26:15 +0700

    7.8 Добавлена ф-ция для проверки доступа юзера к определенному ресурсу

--- src/services/ResourcesService.kt
+++ src/services/ResourcesService.kt
@@ -1,9 +1,14 @@
 package services
 
 import enum.Roles
+import mock.ResoursesMock
 
 class ResourcesService {
     fun findRoles(role: String): Boolean {
         return Roles.values().find { it.roleName == role } != null
+    }
+
+    fun checkResourceAccess(login: String, res: String, role: String): Boolean {
+        return ResoursesMock().resources.find { it.user == login && it.resource == res && it.role == role } != null
     }
 }
